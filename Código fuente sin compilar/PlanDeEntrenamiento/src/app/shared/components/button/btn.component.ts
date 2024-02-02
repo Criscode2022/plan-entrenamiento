ff4172120162def38ef0ff80687deeaf686e82ca
Cristian Damil <cristiancode2023@gmail.com>
2024-02-02 18:24:50 +0100

    Created button component

---
+++ Código fuente sin compilar/PlanDeEntrenamiento/src/app/shared/components/button/btn.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'btnComponent',
+  templateUrl: './btn.component.html',
+  styleUrls: ['./btn.component.scss'],
+})
+export class ButtonComponent {
+  @Input() buttonText: string = '';
+  @Input() buttonColor: 'primary' | 'accent' | 'warn' = 'primary';
+}
--- Código fuente sin compilar/PlanDeEntrenamiento/src/app/shared/shared.module.ts
+++ Código fuente sin compilar/PlanDeEntrenamiento/src/app/shared/shared.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { ButtonComponent } from './components/button/btn.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [ButtonComponent],
+  imports: [CommonModule, MatButtonModule],
+  exports: [ButtonComponent],
 })
 export class SharedModule {}
