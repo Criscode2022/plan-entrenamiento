157ae708c2f7aa638244ff3274cdda292b95b9b3
Cristian Damil <cristiancode2023@gmail.com>
2024-02-02 17:27:45 +0100

    Better types and cleaner CSS

--- Código fuente sin compilar/PlanDeEntrenamiento/src/app/core/indicemasa/indicemasa.component.ts
+++ Código fuente sin compilar/PlanDeEntrenamiento/src/app/core/indicemasa/indicemasa.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { SharedServiceService } from 'src/app/shared/shared-service.service';
+import { numValue, strValue } from 'src/main';
 
 @Component({
   selector: 'app-indicemasa',
@@ -8,36 +9,48 @@ import { SharedServiceService } from 'src/app/shared/shared-service.service';
   styleUrls: ['./indicemasa.component.scss'],
 })
 export class IndicemasaComponent {
-  IMC!: number;
-  peso!: number;
-  altura!: number;
-  colorTextoIMC: string = '';
-  exceso: boolean = false;
+  protected IMC: numValue;
+  protected peso: numValue;
+  protected altura: numValue;
+  protected colorTextoIMC: strValue;
 
   constructor(
     private SharedServiceService: SharedServiceService,
     private snackBar: MatSnackBar
-  ) {}
+  ) {
+    this.IMC = null;
+    this.peso = null;
+    this.altura = null;
+    this.colorTextoIMC = null;
+  }
 
   openSnackBar(message: string, action: string) {
     this.snackBar.open(message, action, { duration: 5000 });
   }
 
-  public calcularIMC(altura: number, peso: number) {
-    const metros = altura / 100;
-    this.IMC = +(peso / (metros * metros)).toFixed(2);
-    this.cambiarColor();
-    this.SharedServiceService.setData(this.IMC);
-    this.SharedServiceService.getData();
+  public calcularIMC(altura: numValue, peso: numValue) {
+    if (altura && peso) {
+      const metros = altura / 100;
+      this.IMC = +(peso / (metros * metros)).toFixed(2);
+      this.cambiarColor();
+      this.SharedServiceService.setData(this.IMC);
+      this.SharedServiceService.getData();
 
-    return this.IMC;
+      return this.IMC;
+    }
+
+    return null;
   }
 
   public cambiarColor() {
-    if (this.IMC >= 25 || this.IMC < 18.5) {
-      this.colorTextoIMC = 'red';
-    } else {
-      this.colorTextoIMC = 'green';
+    if (this.IMC) {
+      if (this.IMC >= 25) {
+        this.colorTextoIMC = 'red';
+      } else if (this.IMC < 18.5) {
+        this.colorTextoIMC = 'red';
+      } else {
+        this.colorTextoIMC = 'green';
+      }
     }
   }
 }
--- Código fuente sin compilar/PlanDeEntrenamiento/src/main.ts
+++ Código fuente sin compilar/PlanDeEntrenamiento/src/main.ts
@@ -5,3 +5,6 @@ import { AppModule } from './app/app.module';
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
+
+export type numValue = number | null;
+export type strValue = string | null;
