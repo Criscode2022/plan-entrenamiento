e15ab20b62171fb81626b968c7724857e500d8f0
Cristian Damil <cristiancode2023@gmail.com>
2024-02-07 20:06:23 +0100

    dark-mode-first-try

--- Código fuente sin compilar/PlanDeEntrenamiento/src/app/core/layout/main/main.component.ts
+++ Código fuente sin compilar/PlanDeEntrenamiento/src/app/core/layout/main/main.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {}
+export class MainComponent {
+  isDarkMode = false;
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+  ) {}
+
+  toggleDarkMode() {
+    const hostElem = this.el.nativeElement;
+    if (this.isDarkMode) {
+      this.renderer.addClass(hostElem, 'dark-mode');
+    } else {
+      this.renderer.removeClass(hostElem, 'dark-mode');
+    }
+    this.isDarkMode = !this.isDarkMode;
+  }
+}
