cc8363e45570d3199ada81affa57ce78b99e2186
Cristian Damil <cristiancode2023@gmail.com>
2026-01-22 22:19:49 +0100

    add missing MatIconModule import

--- src/app/shared/dialogs/dialogo/imc-info-dialog.component.ts
+++ src/app/shared/dialogs/dialogo/imc-info-dialog.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'dialog-content-example-dialog',
   templateUrl: 'imc-info-dialog.component.html',
   styleUrls: ['./imc-info-dialog.component.scss'],
   standalone: true,
-  imports: [MatButtonModule, MatDialogModule],
+  imports: [MatButtonModule, MatDialogModule, MatIconModule],
 })
 export class ImcInfoDialog {}
